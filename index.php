4393372b77b8d37f35007a27d73dad7f27ff53ff
LouiseDiouss <44578819+LouiseDiouss@users.noreply.github.com>
2020-08-11 17:56:51 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Accueil - Taekwondo Challenge</title>
+	<?php include 'includes/css-links.html';?>
+</head>
+<body>
+	<?php include 'includes/menu.php';?>
+
+	<!-- Pieds de page -->
+	<?php include 'includes/footer.php'; ?>
+
+	<?php include 'includes/js-links.html';?>
+</body>
+</html>
