4354715abcece69f313062870623b484ce86ad94
Lisi <samuela.diouf@gmail.com>
2020-08-11 18:08:32 +0200

    Commit initial

---
+++ includes/footer.php
@@ -0,0 +1,47 @@
+<!-- footer -->
+	<div class="mt-5 p-3" style="background-color: grey;">
+		<div class="row">
+			<div class="col-md-4">
+				<h3>A propos</h3>
+				<p>Vous pouvez également nous suivre sur ces réseaux </p>
+				<ul class="agileits_social_list">
+					<li><a href="https://m.facebook.com/taekwondochallenge44/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+					<li><a href="https://twitter.com/taekwondochall1" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+					<li><a href="https://www.instagram.com/taekwondochallenge44/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+				</ul>
+			</div>
+			<div class="col-md-4">
+				<h3>Articles récents</h3>
+				<p>Cette rubrique sera bientôt disponible </p>
+			</div>
+			<div class="col-md-4">
+				<h3>Nos Coordonnées</h3>
+				<ul class="w3_address">
+					<li><i class="fa fa-map-marker" aria-hidden="true"> </i> Siège social: 4 rue Laurent Bonnevay, 44200 Nantes</li>
+					<li><i class="fa fa-map-marker" aria-hidden="true"> </i> Gymnase: esplanade d'angleterre,44200 Nantes</li>
+					<li><i class="fa fa-envelope-o" aria-hidden="true"> </i> <a href="mailto:Taekwondochallenge.44@gmail.com">Taekwondochallenge.44@gmail.com</a></li>
+					<li><i class="fa fa-phone" aria-hidden="true"> </i> (+33) 6 21 46 64 67</li>
+				</ul>
+			</div>
+		</div>
+			<div class="clearfix"> </div>
+			<!-- <div class="row">
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-8">
+					<h3>S'abonner à notre Newsletter</h3>
+					<form action="#" method="post">
+						<input type="email" name="Email" placeholder="Email" required="">
+						<input type="submit" value="Valider">
+					</form>
+				</div>
+				<div class="col-md-2"></div>
+			</div> -->
+			<div class="clearfix"> </div>
+			<div class="row text-center">
+				<span class="text-center">© 2020. All rights reserved | Design by TaekwondoChallenge</span>
+			</div>
+		</div>
+	</div>
+
+<!--fin footer>
---
+++ includes/menu.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="/tc"><img src="assets/media/images/logo.png" alt="Taekwondo-Challenge-logo" style="width: 80px; height: 80px;"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/tc">Accueil <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Le club
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#presentation.php">Présentation</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#infos-pratiques.php">Infos pratiques</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#nous-contacter.php">Nous contacter</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#galerie.php">Galerie</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#prestations.php">Prestations</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#boutique.php">Boutique</a>
+      </li>
+    </ul>
+    <ul class="navbar-text">
+      <li class="nav-item">
+        <a class="nav-link" href="#inscrire.php" role="button">S'inscrire</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link btn btn-default" href="#connexion.php" role="button">Se connecter</a>
+      </li>
+    </ul>
+      
+  </div>
+</nav>
