89fc7362391ae39b4eb20f5cb0978838d1b22419
Lisi <samuela.diouf@gmail.com>
2020-08-13 17:38:58 +0200

    legere modif

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
+<?php 
+	require 'proccess/config.php';
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<title>Accueil - Taekwondo Challenge</title>
 	<?php include 'includes/css-links.html';?>
+	<!--?php include 'includes/style.css';?-->
 </head>
 <body>
 	<?php include 'includes/menu.php';?>
-
-	<p>Bienvenu chez nous !!!</p>
 
 	<!-- Pieds de page -->
 	<?php include 'includes/footer.php'; ?>
---
+++ presentation.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Accueil - Taekwondo Challenge</title>
+	<?php include 'includes/css-links.html';?>
+	<!--?php include 'includes/style.css';?-->
+</head>
+<body>
+	<?php include 'includes/menu.php';?>
+
+		<!-- banner-bottom -->
+			<div class="banner-bottom" >
+				<div class="container">
+					<h2 class="agileits_head">Qui sommes nous?</h2>
+				</div>
+			</div>		
+		<!-- //banner-bottom -->
+				<div class="banner-bottom">
+					<div class="container" >
+						<div align="center"class="embed-responsive embed-responsive-16by9"  >
+							<video controls="autoplay" class="embed-responsive-item" controls poster="assets/media/images/logo.png" >
+								<source src="assets/media/images/vChallenge.mp4" type="video/mp4">
+							</video>
+						</div>	
+							
+						<div class="clearfix"> </div>
+					</div>
+				</div>		
+		<!-- presentation club-->
+		<div class="banner-bottom">
+			<div class="container"  >
+				<div class="presentation_club" style="text-align: justify;">
+					<p style="font-size: 20px;color:black;"> Notre association est une <B>association de sport de combat</B> fondée par l'ancien combattant <B>Aly Sissoko</B> en 2018.<br/>
+						Le coeur de l'association est donc de faciliter l'accès à la pratique du taekwondo auprès
+						<ul > 
+							<li style="font-size:20px;margin-left:4vw;">des jeunes des quartiers défavorisés,</li> 
+							<li style="font-size:20px;margin-left:4vw;">du public féminin </li>
+							<li style="font-size:20px;margin-left:4vw;">et des adolescents.</li>
+						</ul>
+					 </p>
+					 <p style="font-size: 20px;color:black;">  
+						Lors de nos actions avec nos partenaires, nous avons pu rencontrer des groupes de jeunes en réinsertion.<br/>
+						Ceux-ci ont apprécié ce sport de combat pour leurs besoins personnels et professionnels.<br/>
+						Pour l'année 2020, l'association Challenge Taekwondo organise des prestations et des actions d'animations et de développement.
+							
+					</p>
+				</div>
+			</div>
+		</div>
+		<!-- //presentation club-->
+
+	<!-- Pieds de page -->
+	<?php include 'includes/footer.php'; ?>
+
+	<?php include 'includes/js-links.html';?>
+</body>
+</html>
