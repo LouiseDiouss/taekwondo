09c274883a39fac4a5d91b11a65b4c1ccaf3daa2
lekaiser <sylvanus010@gmail.com>
2020-08-18 23:29:31 +0200

    Paramètres, menu-admin, compte admin

--- admin/includes/menu-admin.php
+++ admin/includes/menu-admin.php
@@ -6,19 +6,44 @@
  * Time: 13:14
  */
 ?>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="height:">
-    <ul class="nav flex-column">
-      <li class="nav-item">
-        <a class="nav-link active" href="#">Prestations</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="admin/reservations/list-reservation.php">Reservations</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Messages de contact</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Utilisateurs</a>
-      </li>
-    </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="/"><img src="/assets/media/images/logo.png" alt="Taekwondo-Challenge-logo" style="width: 80px; height: 80px;"></a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
+            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Accueil <span class="sr-only">(current)</span></a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="../prestations/list-prestation.php">Prestations</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../reservations/list-reservation.php">Reservations</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#../contacts/list-contact.php">Messages de contacts</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../users/list-user.php">Utilisateurs</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../parametres.php">Paramètres</a>
+            </li>
+           <!-- <li class="nav-item">
+                <a class="nav-link" href="../users/list-user.php">Utilisateurs</a>
+            </li>-->
+        </ul>
+
+        <div class="navbar-text">
+            <a href="../mon-compte.php" class="btn btn-success" role="button">Mon compte</a>
+            <a href="../../logout.php" class="btn btn-warning" role="button">
+                <i class="fa fa-power-off" aria-hidden="true"></i>
+            </a>
+        </div>
+    </div>
 </nav>
---
+++ admin/users/list-user.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Liste des utilisateurs - Taekwondo Challenge</title>
+    <?php include '../../includes/css-links.html';?>
+</head>
+<body>
+    <?php include '../../includes/menu.php';?>
+</body>
+</html>
