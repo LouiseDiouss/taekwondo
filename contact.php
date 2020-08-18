f1dddbf75f2099cde019fe9491cf1bc05c3001a6
Lisi <samuela.diouf@gmail.com>
2020-08-18 23:37:35 +0200

    m

--- contact.php
+++ contact.php
@@ -170,43 +170,7 @@
 			</div>
 		</div>
 	</div>
-<!-- //bootstrap-pop-up -->
-<!-- for bootstrap working -->
-	<!--script src="assets/js/bootstrap.js"></script-->
-<!-- //for bootstrap working -->
-<!-- start-smoth-scrolling -->
-<!--script type="text/javascript" src="./assets/js/move-top.js"></script>
-<script type="text/javascript" src="./assets/js/easing.js"></script>
-<script type="text/javascript">
-	jQuery(document).ready(function($) {
-		$(".scroll").click(function(event){		
-			event.preventDefault();
-			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
-		});
-	});
-</script>
-<start-smoth-scrolling -->
-<!-- here stars scrolling icon -->
-	<!--script type="text/javascript">
-		$(document).ready(function() {
-			/*
-				var defaults = {
-				containerID: 'toTop', // fading element id
-				containerHoverID: 'toTopHover', // fading element hover id
-				scrollSpeed: 1200,
-				easingType: 'linear' 
-				};
-			*/
-								
-			$().UItoTop({ easingType: 'easeOutQuart' });
-								
-			});
-	</script>
-<//here ends scrolling icon -->
 
-		
-
-	<!-- infos  page contact -->
 	<!-- Pieds de page -->
 	<?php include 'includes/footer.php'; ?>
 
