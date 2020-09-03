e59a685c1e8379d9696a57fb4c49b4091c4bbd57
Lisi <samuela.diouf@gmail.com>
2020-09-03 11:43:52 +0200

    mise à jour espace membre + nouveau menu du site pour le membre

---
+++ includes/menu_site_Membre.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="">
+  <!--a class="navbar-brand" href="/"><img src="assets/media/images/logo.png"  width="100px" height="90px" alt="Taekwondo-Challenge-logo"></a-->
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    <!--img src=".\images\logo.png"-->
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/taekwondo/index.php">Accueil <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Le club
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="/taekwondo/presentation.php">Présentation</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="/taekwondo/infos.php">Infos pratiques</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="/taekwondo/contact.php">Nous contacter</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/taekwondo/galerie.php">Galerie</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/taekwondo/prestations.php">Prestations</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/taekwondo/boutique.php">Boutique</a>
+      </li>
+    </ul>
+      
+  </div>
+</nav>
--- mon-espace.php
+++ mon-espace.php
@@ -13,8 +13,8 @@
 <body>
 
     <?php include 'includes/menuMembre.php';?>
-    <section style="background:pink;background-size:100%;color:black;text-transform: capitalize;position:relative;width:1116px;left:250px;padding:0px 5px 10px 0px;">
-    	<?php include 'admin/includes/menu-admin.php';?>
+    <section style="background-size:100%;color:black;text-transform: capitalize;position:relative;width:1116px;left:250px;padding:0px 5px 10px 0px;">
+    	<?php include 'includes/menu_site_Membre.php';?>
     	<!-- Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.-->
     </section>
     <!-- Pieds de page -->
